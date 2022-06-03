b8f7b8481643b53163d5ad29712f0aa546f3c15f
izharkevich <izharkevich@clevertec.ru>
2022-06-03 10:06:24 +0300

    react-router-v6

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export const Homepage = () => {
   return <h1>HomePage</h1>;
 };
--- src/pages/Singlepage.jsx
+++ src/pages/Singlepage.jsx
@@ -1,9 +1,13 @@
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 export const Singlepage = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
   const [post, setPost] = useState(null);
+
+  const goBack = () => navigate(-1);
+  const goHome = () => navigate("/", { replace: true });
 
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
@@ -15,12 +19,16 @@ export const Singlepage = () => {
     <div>
       {post && (
         <>
-          <h1>{post.title}</h1>
-          <p>{post.body}</p>
-          <Link to={`/posts/${id}/edit`}>Edit this post</Link>
-          <div>
-            <Link to={`/posts/new`}>Create a post</Link>
-          </div>
+          <button onClick={goBack}>Go back</button>
+          {/* Bad approach */}
+          <button onClick={goHome}>Go home</button>
+          {post && (
+            <>
+              <h1>{post.title}</h1>
+              <p>{post.body}</p>
+              <Link to={`/posts/${id}/edit`}>Edit this post</Link>
+            </>
+          )}
         </>
       )}
     </div>
